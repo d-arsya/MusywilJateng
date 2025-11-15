afeb1939f73aa70121471390dd320fe04a258976
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-11-16 05:04:45 +0700

    fix: Force root URL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Auth\StaticAdminProvider;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+            URL::forceRootUrl(config('app.url'));
+        }
         Auth::provider('static_admin', function ($app, array $config) {
             return new StaticAdminProvider();
         });
