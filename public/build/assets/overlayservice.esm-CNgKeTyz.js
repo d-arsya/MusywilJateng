26b029465dae5461c3d22ce5c15ed5a005db2f70
d-arsya <kamaluddin.arsyad17@gmail.com>
2025-12-20 11:54:13 +0700

    fix: Add barcode and photo

--- public/build/assets/overlayservice.esm-CNgKeTyz.js
+++
@@ -1,1 +0,0 @@
-import{Q as r}from"./app-DjjRU0sr.js";var a=r();export{a as O};
--- resources/js/pages/card.jsx
+++ resources/js/pages/card.jsx
@@ -43,7 +43,7 @@ export default function CardPage({ auth, app }) {
                 {/* Card container */}
                 <div ref={cardRef} className="relative w-full max-w-xl">
                     {/* Background ID Card */}
-                    <img src="/assets/img/verti.jpeg" alt="Banner" className="w-full rounded-lg shadow-md" />
+                    <img src="/assets/img/vertic.jpeg" alt="Banner" className="w-full rounded-lg shadow-md" />
 
                     {/* Overlay content */}
                     <div className="absolute top-1/5 left-1/2 flex w-full -translate-x-1/2 transform flex-col items-center space-y-3">
